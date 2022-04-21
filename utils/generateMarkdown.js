de3640fd7b567092cbc936d788e1c0b37dca4520
Keerti Chaudhary <17keerti@gmail.com>
2022-04-20 21:51:01 -0700

    write a function to return licence badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![Badge](https://img.shields.io/badge/License-${license}-blue)`
+}
 
 
 // TODO: Create a function that returns the license link
